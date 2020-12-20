3ec8e7fd9f59482f9075b54fcf726bff4ea79493
shwetabh <shwetabh.srivastava@gmail.com>
2020-12-20 11:12:47 +0530

    dd

---
+++ ResolveConflicts/src/Demo/Test.java
@@ -0,0 +1,17 @@
+package Demo;
+
+public class Test {
+	
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Hello");
+		int data1=10;
+		
+		int data2=20;
+		
+		System.out.println(data1+data2);
+	}
+
+}
